c13fd304c5c2ce3eefbadfeb5d9cc46f0f3d1859
Kuanysh <anuarbekov.kuanysh@mail.ru>
2021-12-17 13:32:27 +0600

    changed

--- lesson_1/home_work_1.py
+++ lesson_1/home_work_1.py
@@ -2,20 +2,24 @@ dur = int(input('Enter duration: '))
 
 if dur < 60:
     second = dur % 60
+    print('duration =', dur)
     print(second, 'сек')
 elif dur < 3600:
     minute = dur // 60
     second = dur % 60
+    print('duration =', dur)
     print(minute, 'мин', second, 'сек')
 elif dur < 86400:
     hour = dur // 3600
     minute = dur % 3600 // 60
     second = dur % 60
+    print('duration =', dur)
     print(hour, 'час', minute, 'мин', second, 'сек')
 else:
     day = dur // 86400
     hour = dur % 86400 // 3600
     minute = dur % 3600 // 60
     second = dur % 60
+    print('duration =', dur)
     print(day, 'дн', hour, 'час', minute, 'мин', second, 'сек')
 
